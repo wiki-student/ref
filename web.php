d856638f83852013e8475bca20ff4053bf85cf00
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-02-15 18:18:07 +0300

    Update web.php
    create table for data, add using utf-8

--- web.php
+++ web.php
@@ -1,16 +1,26 @@
-<?php 
-   require_once("config.php");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+</head>
+<body>
+  <table border="1">   
+    <?php 
+    require_once("config1.php");
     $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     if (!$link) {
         echo 'I can not connect to the database. Error code: ' . mysqli_connect_error() . ', error: ' . mysqli_connect_error();
         exit;
       }
-      $sql = mysqli_query($link, 'SELECT `adaptive_bandwidth`, `a_frames_decoded`, `a_frames_dropped`, `a_frames_failed`, `avg_bitrate`, `begin`,`discontinuties`,`end`,`id`,`proto`,`timestamp`,`type`,`v_frames_decoded`, `v_frames_dropped`, `v_frames_failed`, 
-      `f_gateway`, `f_ip`, `f_name`, `f_netmask`, `f_speed`, `f_s_received_bytes`, `f_s_received_discard_packets`, `f_s_received_error_packets`, `f_s_received_multicast_packets`, `f_s_received_total_packets`, `f_s_sent_bytes`, `f_s_sent_error_packets`, `f_s_sent_total_packets`, `f_timestamp`, `f_type`,
-      `t_gateway`, `t_ip`, `t_name`, `t_netmask`, `t_speed`, `t_s_received_bytes`, `t_s_received_discard_packets`, `t_s_received_error_packets`, `t_s_received_multicast_packets`, `t_s_received_total_packets`, `t_s_sent_bytes`, `t_s_sent_error_packets`, `t_s_sent_total_packets`, `t_timestamp`, `t_type`  FROM `data`');
+      $sql = mysqli_query($link, 'SELECT begin, a_frames_decoded, end, type FROM data');?>
+      <tr><th>begin</th><th>a_frames_decoded</th><th>end</th><th>type</th></tr>
+      <?php 
       while ($result = mysqli_fetch_array($sql)) {
-        echo "{$result['adaptive_bandwidth']} {$result['a_frames_decoded']} {$result['a_frames_dropped']} {$result['avg_bitrate']} {$result['begin']} {$result['discontinuties']} {$result['end']} {$result['id']} {$result['proto']} {$result['timestamp']} {$result['type']} {$result['v_frames_decoded']} {$result['v_frames_dropped']} {$result['v_frames_failed']} 
-        {$result['f_duplex']} {$result['f_gateway']} {$result['f_ip']} {$result['f_name']} {$result['f_netmask']} {$result['f_speed']} {$result['f_s_received_bytes']} {$result['f_s_received_discard_packets']} {$result['f_s_received_error_packets']} {$result['f_s_received_multicast_packets']} {$result['f_s_received_total_packets']} {$result['f_s_sent_bytes']} {$result['f_s_sent_error_packets']} {$result['f_s_sent_total_packets']} {$result['f_timestamp']} {$result['f_type']}
-        {$result['t_duplex']} {$result['t_gateway']} {$result['t_ip']} {$result['t_name']} {$result['t_netmask']} {$result['t_speed']} {$result['t_s_received_bytes']} {$result['t_s_received_discard_packets']} {$result['t_s_received_error_packets']} {$result['t_s_received_multicast_packets']} {$result['t_s_received_total_packets']} {$result['t_s_sent_bytes']} {$result['t_s_sent_error_packets']} {$result['t_s_sent_total_packets']} {$result['t_timestamp']} {$result['t_type']} <br>";
+        echo "<tr><td>{$result['begin']}</td><td>{$result['a_frames_decoded']}</td><td>{$result['end']}</td><td>{$result['type']}</td></tr>";
       }
-?>
+  ?>
+</body>
+</html>
+
