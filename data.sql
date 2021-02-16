7b8f739fc28f304aca447b95f1dd6c72659b0c3b
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-02-16 18:35:33 +0300

    deleted many values, changed name of strings, and type

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 16 2021 г., 12:15
+-- Время создания: Фев 16 2021 г., 19:33
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -27,15 +27,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `data` (
-  `IP` int(11) NOT NULL,
+  `IP` int(11) unsigned NOT NULL,
   `adaptive_bandwidth` int(11) NOT NULL,
   `a_frames_decoded` int(11) NOT NULL,
   `a_frames_dropped` int(11) NOT NULL,
   `a_frames_failed` int(11) NOT NULL,
   `avg_bitrate` int(11) NOT NULL,
-  `begin` bigint(11) NOT NULL,
+  `begin` int(11) NOT NULL,
   `discontinuties` int(11) NOT NULL,
-  `end` bigint(11) NOT NULL,
+  `end` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `proto` text NOT NULL,
   `timestamp` int(11) NOT NULL,
@@ -44,38 +44,22 @@ CREATE TABLE IF NOT EXISTS `data` (
   `v_frames_decoded` int(11) NOT NULL,
   `v_frames_dropped` int(11) NOT NULL,
   `v_frames_failed` int(11) NOT NULL,
-  `f_duplex` text NOT NULL,
-  `f_gateway` text NOT NULL,
-  `f_ip` text NOT NULL,
-  `f_name` text NOT NULL,
-  `f_netmask` text NOT NULL,
-  `f_speed` int(11) NOT NULL,
-  `f_s_received_bytes` bigint(11) NOT NULL,
-  `f_s_received_discard_packets` int(11) NOT NULL,
-  `f_s_received_error_packets` int(11) NOT NULL,
-  `f_s_received_multicast_packets` int(11) NOT NULL,
-  `f_s_received_total_packets` bigint(11) NOT NULL,
-  `f_s_sent_bytes` bigint(11) NOT NULL,
-  `f_s_sent_error_packets` int(11) NOT NULL,
-  `f_s_sent_total_packets` int(11) NOT NULL,
-  `f_timestamp` bigint(11) NOT NULL,
-  `f_type` text NOT NULL,
-  `t_duplex` text NOT NULL,
-  `t_gateway` text NOT NULL,
-  `t_ip` text NOT NULL,
-  `t_name` text NOT NULL,
-  `t_netmask` text NOT NULL,
-  `t_speed` int(11) NOT NULL,
-  `t_s_received_bytes` bigint(11) NOT NULL,
-  `t_s_received_discard_packets` int(11) NOT NULL,
-  `t_s_received_error_packets` int(11) NOT NULL,
-  `t_s_received_multicast_packets` int(11) NOT NULL,
-  `t_s_received_total_packets` bigint(11) NOT NULL,
-  `t_s_sent_bytes` bigint(11) NOT NULL,
-  `t_s_sent_error_packets` int(11) NOT NULL,
-  `t_s_sent_total_packets` int(11) NOT NULL,
-  `t_timestamp` bigint(11) NOT NULL,
-  `t_type` text NOT NULL
+  `duplex` text NOT NULL,
+  `gateway` int(11) unsigned NOT NULL,
+  `IP_net` int(11) unsigned NOT NULL,
+  `name` text NOT NULL,
+  `netmask` int(11) unsigned NOT NULL,
+  `speed` int(11) NOT NULL,
+  `received_bytes` bigint(11) NOT NULL,
+  `received_discard_packets` int(11) NOT NULL,
+  `received_error_packets` int(11) NOT NULL,
+  `received_multicast_packets` int(11) NOT NULL,
+  `received_total_packets` bigint(11) NOT NULL,
+  `sent_bytes` bigint(11) NOT NULL,
+  `sent_error_packets` int(11) NOT NULL,
+  `sent_total_packets` int(11) NOT NULL,
+  `timestamp_net` int(11) NOT NULL,
+  `type_net` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
