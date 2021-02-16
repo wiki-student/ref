392f844d1567f428544a685af532b9e440282cd5
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-02-16 13:08:01 +0300

    add css elements for table, fixed name config

--- web.php
+++ web.php
@@ -6,21 +6,62 @@
     <title>Page Title</title>
 </head>
 <body>
-  <table border="1">   
+  <table>   
     <?php 
-    require_once("config1.php");
+    require_once("config.php");
     $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     if (!$link) {
         echo 'I can not connect to the database. Error code: ' . mysqli_connect_error() . ', error: ' . mysqli_connect_error();
         exit;
       }
-      $sql = mysqli_query($link, 'SELECT begin, a_frames_decoded, end, type FROM data');?>
-      <tr><th>begin</th><th>a_frames_decoded</th><th>end</th><th>type</th></tr>
+      $sql = mysqli_query($link, 'SELECT IP, f_name, f_speed, timestamp, a_frames_failed/a_frames_decoded*100 AS "a", timestamp, v_frames_failed/v_frames_decoded*100 AS "v" FROM data');?>
+      <tr><th>IP</th><th>Network Name</th><th>Network speed</th><th>Timestamp</th><th>Audio errors</th><th>Video errors</th></tr>
       <?php 
       while ($result = mysqli_fetch_array($sql)) {
-        echo "<tr><td>{$result['begin']}</td><td>{$result['a_frames_decoded']}</td><td>{$result['end']}</td><td>{$result['type']}</td></tr>";
+        echo "<tr><td>{$result['IP']}</td><td>{$result['f_name']}</td><td>{$result['f_speed']}</td><td>{$result['timestamp']}</td><td>{$result['a']}</td><td>{$result['v']}</td></tr>";
       }
   ?>
 </body>
+<style>
+  *{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+  }
+  table {
+  font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+  font-size: 14px;
+  border-radius: 10px;
+  border-spacing: 0;
+  text-align: center;
+  margin:0px auto;
+  
+  }
+  th {
+  background: #BCEBDD;
+  color: black;
+  padding: 10px 20px;
+  position: sticky;
+  top:0px;
+  }
+  th, td {
+  border-style: solid;
+  border-width: 0 1px 1px 0;
+  border-color: white;
+  }
+  th:first-child, td:first-child {
+  text-align: left;
+  }
+  th:last-child {
+  border-right: none;
+  }
+  td {
+  padding: 10px 20px;
+  background: #F8E391;
+  }
+  tr td:last-child {
+  border-right: none;
+  }
+</style>
 </html>
 
