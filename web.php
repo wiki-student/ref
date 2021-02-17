0641ed5448fedca0de853302e13e50e5d5d1a066
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-02-17 17:36:27 +0300

    add counter for rows

--- web.php
+++ web.php
@@ -14,53 +14,62 @@
         echo 'I can not connect to the database. Error code: ' . mysqli_connect_error() . ', error: ' . mysqli_connect_error();
         exit;
       }
-      $sql = mysqli_query($link, 'SELECT IP, name, speed, FROM_UNIXTIME(timestamp) AS time, a_frames_failed/a_frames_decoded*100 AS "a", v_frames_failed/v_frames_decoded*100 AS "v" FROM data LIMIT 500');?>
+      $sql = mysqli_query($link, 'SELECT IP, name, speed, FROM_UNIXTIME(timestamp) AS time, a_frames_failed/a_frames_decoded*100 AS "a", v_frames_failed/v_frames_decoded*100 AS "v" FROM data LIMIT 500');
+      $count = mysqli_query($link, 'SELECT * FROM data');?>
       <tr><th>IP</th><th>Network Name</th><th>Network speed</th><th>Timestamp</th><th>Audio errors</th><th>Video errors</th></tr>
       <?php 
       while ($result = mysqli_fetch_array($sql)) {
         echo "<tr><td>".long2ip($result['IP'])."</td><td>{$result['name']}</td><td>{$result['speed']}</td><td>{$result['time']}</td><td>{$result['a']}</td><td>{$result['v']}</td></tr>";
-      };
-  ?>
+      };?>
+      <span>Total number of rows: <?php print_r ($count->num_rows);?></span>
+    </table>
 </body>
 <style>
   *{
     margin: 0;
     padding:0;
     box-sizing: border-box;
-  }
-  table {
-  font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
-  font-size: 14px;
-  border-radius: 10px;
-  border-spacing: 0;
-  text-align: center;
-  margin:0px auto;
-  
-  }
-  th {
-  background: #BCEBDD;
-  color: black;
-  padding: 10px 20px;
-  position: sticky;
-  top:0px;
-  }
-  th, td {
-  border-style: solid;
-  border-width: 0 1px 1px 0;
-  border-color: white;
-  }
-  th:first-child, td:first-child {
-  text-align: left;
-  }
-  th:last-child {
-  border-right: none;
-  }
-  td {
-  padding: 10px 20px;
-  background: #F8E391;
-  }
-  tr td:last-child {
-  border-right: none;
-  }
+    }
+    table {
+    font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+    font-size: 14px;
+    border-radius: 10px;
+    border-spacing: 0;
+    text-align: center;
+    margin:-21px auto 0 auto;
+    }
+    th {
+    background: #BCEBDD;
+    color: black;
+    padding: 10px 20px;
+    position: sticky;
+    top:0px;
+    }
+    th, td {
+    border-style: solid;
+    border-width: 0 1px 1px 0;
+    border-color: white;
+    }
+    th:first-child, td:first-child {
+    text-align: left;
+    }
+    th:last-child {
+    border-right: none;
+    }
+    td {
+    padding: 10px 20px;
+    background: #F8E391;
+    }
+    tr td:last-child {
+    border-right: none;
+    }
+    span{
+    font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+    font-size: 14px;
+    position: sticky;
+    top:0px;
+    background-color:#BCEBDD;
+    padding: 10px 5px;    
+    }
 </style>
 </html>
