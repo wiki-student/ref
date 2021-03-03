18faba4c32dce7be1203fb4eaec2773aece3c359
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-03-03 17:08:18 +0300

    added top 5 channels

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <body>
     <p>
       <form action="web.php" method="post">
-        From: <input type="text" name="from"/>
-        To: <input type="text" name="to"/>
+        From: <input type="datetime-local" name="from"/>
+        To: <input type="datetime-local" name="to"/>
         IP: <input type="text" name="ip"/>
         <input type="submit"/>
       </form>
@@ -23,11 +23,17 @@
       }
       $r_count = mysqli_query($link, 'SELECT count(IP) FROM data');
       $uniq_IP = mysqli_query($link, 'SELECT count(DISTINCT IP) FROM data ');
-      $row= $r_count->fetch_array();
+      $top = mysqli_query($link, 'SELECT url, COUNT(*) FROM data GROUP BY url order BY COUNT(*) DESC');
+      $row= $r_count->fetch_array();      
       echo "Total number of rows:".$row[0]."<br>";
       $row= $uniq_IP->fetch_array();
       echo "Unique IP:".$row[0]."<br>";
+      while($i<=4){
+        $row= mysqli_fetch_array($top);        
+        echo $row['COUNT(*)'].' ';
+        echo $row['url']."<br>";
+        $i++;
+      }
     ?>
-  </table>
 </body>
 </html>
