6f6adea1af410c6f7fe5923d16b92efb50d8a9e2
wiki-student <78481336+wiki-student@users.noreply.github.com>
2021-03-04 16:36:26 +0300

    added top sites for data and ip

--- web.php
+++ web.php
@@ -26,7 +26,7 @@
         $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
           if (!$link) 
           {
-          echo 'I can not connect to the database. Error code: ' . mysqli_connect_error() . '';
+          echo 'I can not connect to the database. Error code:' . mysqli_connect_error() . '';
           exit;
           }   
         if($from==''){
@@ -36,9 +36,24 @@
           $to=time();
         }
         if($ip==''){
-          $sql= 'SELECT * FROM data WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' limit 500';          
+          $sql= 'SELECT * FROM data 
+            WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' limit 500';
+          $top = mysqli_query($link, 'SELECT url, COUNT(*) FROM data 
+            WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' 
+            GROUP BY url order BY COUNT(*) DESC');          
         }else{  
-        $sql= 'SELECT *FROM data WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' AND IP= '.$ip.' limit 500';}
+          $sql= 'SELECT *FROM data 
+            WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' AND IP= '.$ip.' limit 500';
+          $top = mysqli_query($link, 'SELECT url, COUNT(*) FROM data 
+            WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' AND IP= '.$ip.' 
+            GROUP BY url order BY COUNT(*) DESC');}
+        echo 'Top sites:'."</br>";
+        while($i<=4){
+          $row= mysqli_fetch_array($top);        
+          echo $row['COUNT(*)'].' ';
+          echo $row['url']."<br>";
+          $i++;
+        }
         $result = mysqli_query($link, $sql);
     ?>
         <tr>
@@ -79,7 +94,7 @@
         $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
         if (!$link) 
         {
-          echo 'I can not connect to the database. Error code: ' . mysqli_connect_error() . '';
+          echo 'I can not connect to the database. Error code:' . mysqli_connect_error() . '';
           exit;
         }
         $sql='SELECT IP,
