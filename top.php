627f04bf1a01c4cb1a86e2ac70a1de1753cb5625
Илья Калужа <527ilyxa@gmail.com>
2021-03-11 16:35:16 +0300

    count now uniq ip

--- top.php
+++ top.php
@@ -12,20 +12,20 @@
         IP: <input type="text" name="ip"/>
         <input type="submit"/>
       </form>
-    </p>    
+    </p>
     <?php
     $from =strtotime($_POST['from']);
     $to = strtotime($_POST['to']);
     $ip = ip2long($_POST['ip']);
     require_once("config.php");
     if($from<>'' || $to<>'' || $ip<>'')
-    { 
+    {
         $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-          if (!$link) 
+          if (!$link)
           {
           echo 'I can not connect to the database. Error code:' . mysqli_connect_error() . '';
           exit;
-          }   
+          }
         if($from==''){
           $from=1;
         }
@@ -33,17 +33,19 @@
           $to=time();
         }
         if($ip==''){
-          $top = mysqli_query($link, 'SELECT url, COUNT(*) FROM data
-           WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' 
-            GROUP BY url order BY COUNT(*) DESC');          
-        }else{  
-          $top = mysqli_query($link, 'SELECT url, COUNT(*) FROM data 
-           WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' AND IP= '.$ip.' 
-            GROUP BY url order BY COUNT(*) DESC');}
+          $top = mysqli_query($link, 'SELECT url, COUNT(url)  FROM
+           (SELECT DISTINCT ip,url FROM data
+             WHERE timestamp>= '.$from.' AND timestamp<= '.$to.') AS q
+              GROUP BY url order BY COUNT(url) DESC');
+        }else{
+          $top = mysqli_query($link, 'SELECT url, COUNT(url)  FROM
+           (SELECT DISTINCT ip,url FROM data
+            WHERE timestamp>= '.$from.' AND timestamp<= '.$to.' and IP= '.$ip.') AS q
+             GROUP BY url order BY COUNT(url) DESC');}
         echo 'Top sites:'."</br>";
         while($i<=24){
-          $row= mysqli_fetch_array($top);        
-          echo $row['COUNT(*)'].' ';
+          $row= mysqli_fetch_array($top);
+          echo $row['COUNT(url)'].' ';
           echo $row['url']."<br>";
           $i++;
         }
